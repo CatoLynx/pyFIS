c808236ded5a955f29b1cdc671ed80f08336243d
Julian Metzler <git@mezgr.de>
2020-10-02 20:45:26 +0200

    Add calibration to K9000 toolkit and rename script

--- caltest.py
+++
@@ -1,5 +0,0 @@
-from pyfis.krone import *
-from pyfis.krone.util import *
-
-fbm = KroneFBMController("/dev/ttyUSB5")
-calibrate_fbm_interactive(fbm, 0)
--- k9000_fbm_toolkit.py
+++ k9000_fbm_toolkit.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 
 from pyfis.krone import Krone9000FBM
+from pyfis.krone.util import calibrate_fbm_interactive
 
 
 def read_value_table(filename):
@@ -34,7 +35,6 @@ def read_value_table(filename):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=str, required=True)
-    parser.add_argument("-a", "--addr", type=int, required=True)
     parser.add_argument("-t", "--table", type=str, required=False)
     args = parser.parse_args()
     
@@ -46,10 +46,21 @@ def main():
         table = None
     
     while True:
-        action = input("What to do? [Code/Status/Letter/Home/WrtTbl/DelTbl/Exit] ").upper()
-        if action.startswith("C"):
+        data = input("What to do? [Code/Status/Letter/Text/Home/WrtTbl/DelTbl/caliBrate/Exit] ").upper()
+        if not data:
+            continue
+        action = data[0]
+        if action not in ("H", "E"):
+            if len(data) < 2:
+                print("  No address specified!")
+                continue
+            if action == "L":
+                addr = int(data[1:-1])
+            else:
+                addr = int(data[1:])
+        if action == "C":
             sys.stdout.write("  FBM Code: ")
-            code = fbm.read_code(args.addr)
+            code = fbm.read_code(addr)
             if len(code) < 1:
                 print("No data received!")
                 continue
@@ -62,38 +73,47 @@ def main():
                 print(f"Unknown status code 0x{code:02X}")
             else:
                 print(chr(code))
-        elif action.startswith("S"):
+        elif action == "S":
             sys.stdout.write("  FBM Status: ")
             try:
-                print(fbm.get_status(args.addr))
+                print(fbm.get_status(addr))
             except:
                 print("No data received!")
                 continue
-        elif action.startswith("L"):
-            if len(action) < 2:
+        elif action == "L":
+            if len(data) < 3:
                 print("  No letter specified!")
                 continue
-            letter = action[1]
-            fbm.set_code(args.addr, ord(letter[0]))
+            letter = data[-1]
+            fbm.set_code(addr, ord(letter[0]))
             fbm.set_all()
-        elif action.startswith("H"):
+        elif action == "T":
+            text = input("  Enter text: ").upper()
+            fbm.set_text(text, addr)
+            fbm.set_all()
+        elif action == "H":
             print("  Rotating to home position")
             fbm.set_home()
-        elif action.startswith("W"):
+        elif action == "W":
             if not table:
                 print("  No value table loaded!")
                 continue
             print("  Writing value table")
             for pos, value in table.items():
                 print(f"    Writing flap {pos}: {chr(value)}")
-                resp = fbm.set_table(args.addr, value, pos)[0]
-        elif action.startswith("D"):
+                resp = fbm.set_table(addr, value, pos)[0]
+        elif action == "D":
             if not table:
                 print("  No value table loaded!")
                 continue
             print("  Deleting value table")
-            resp = fbm.delete_table(args.addr)[0]
-        elif action.startswith("E"):
+            resp = fbm.delete_table(addr)[0]
+        elif action == "B":
+            if addr != 0:
+                print("  Calibration is only possible for address 0!")
+                continue
+            calibrate_fbm_interactive(fbm, addr)
+        elif action == "E":
             break
         
 
