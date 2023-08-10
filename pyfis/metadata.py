d0640daa7e7ac8191e48a410b5577beafdf93d01
Julian Metzler <git@mezgr.de>
2023-08-10 10:16:10 +0200

    Bump version

--- pyfis/metadata.py
+++ pyfis/metadata.py
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 name = "pyFIS"
-version = "1.11.1"
+version = "1.12.0"
 description = "A library for controlling devices in the passenger information realm"
 license = "GPLv3"
 author = "Julian Metzler"
