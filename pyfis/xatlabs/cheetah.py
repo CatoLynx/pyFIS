dbd65625800c9bc11f9d4b6452beae4f250b2cd6
Julian Metzler <git@mezgr.de>
2024-07-16 21:48:15 +0200

    xatLabs Cheetah: Breaking change - update to reflect new buffer structure, add 1bpp pixel buffer support

--- pyfis/xatlabs/cheetah.py
+++ pyfis/xatlabs/cheetah.py
@@ -18,6 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import base64
 import requests
 
+from PIL import Image
+
 from ..splitflap_display import SplitFlapDisplay, TextField, CustomMapField
 
 
@@ -28,8 +30,8 @@ class xatLabsCheetah:
     transmitted as a Base64 encoded string.
     The reason it's done this was is to allow transferring the
     framebuffer inside a JSON object.
-    Cheetah supports three types of displays:
-    - Pixel-based displays (not yet implemented here)
+    Cheetah supports various types of displays, including:
+    - Pixel-based displays (Only 1bpp pixel buffer supported so far)
     - Character-based displays (e.g. a character LCD or alphanumerical split-flap display)
     - Selection-based displays (e.g. a rolling film or generic split-flap display)
     """
@@ -43,61 +45,24 @@ class xatLabsCheetah:
             self.load_device_info()
         elif self.display_info is None or self.device_info is None:
             raise ValueError("Either host or display_info and device_info must be given")
-        self.init_framebuf()
+        self.init_buffers()
 
-    def init_framebuf(self):
-        display_type = self.display_info.get('type')
-        if display_type == 'character':
-            self.framebuf = [0] * self.display_info["charbuf_size"]
-        elif display_type == 'selection':
-            self.framebuf = [0] * self.display_info["framebuf_size"]
-            if 'units' in self.display_info['config']:
-                for unit in self.display_info['config']['units']:
-                    if 'addr' in unit and 'home' in unit:
-                        self.framebuf[unit['addr']] = unit['home']
-        else:
-            raise NotImplementedError("Unsupported display type: {}".format(display_type))
-
-    def get_actual_char_count(self, s):
-        count = 0
-        if "combining_full_stop" in self.display_info["quirks"]:
-            for i in range(len(s)):
-                if i == 0:
-                    count += 1  # Always count first character
-                elif s[i] == ".":
-                    if s[i-1] == ".":
-                        count += 1  # Only count full stop if preceded by another full stop
-                else:
-                    count += 1  # Always count non full stop characters
-        else:
-            count = len(s)
-        return count
-
-    def update_framebuf_text(self, text):
-        self.init_framebuf()
-        val = ""
-        for e in text.split("\n"):
-            actual_len = self.get_actual_char_count(e)
-            excess = actual_len - self.display_info["width"]
-            if excess < 0:
-                # Need to pad
-                val += e.ljust(len(e) - excess, " ")
-            else:
-                # Need to cut off
-                cutoff_pos = len(e) - excess
-                val += e[:cutoff_pos]
-        val_len = len(val)
-        for i in range(self.display_info["charbuf_size"]):
-            if i >= val_len:
-                self.framebuf[i] = 0
-            else:
-                code = ord(val[i])
-                self.framebuf[i] = code if code <= 255 else 0
-
-    def update_framebuf_sel(self, module_data):
-        self.init_framebuf()
-        for pos, val in module_data.items():
-            self.framebuf[pos] = int(val)
+    def init_buffers(self):
+        self.pixel_buffer = None
+        self.text_buffer = None
+        self.unit_buffer = None
+        
+        pixbuf_size = self.display_info.get('pixbuf_size')
+        if pixbuf_size is not None:
+            self.pixel_buffer = [0] * pixbuf_size
+        
+        textbuf_size = self.display_info.get('textbuf_size')
+        if textbuf_size is not None:
+            self.text_buffer = [0] * textbuf_size
+        
+        unitbuf_size = self.display_info.get('unitbuf_size')
+        if unitbuf_size is not None:
+            self.unit_buffer = [0] * unitbuf_size
 
     def load_display_info(self):
         resp = requests.get(f"{self.host}/info/display.json")
@@ -107,18 +72,54 @@ class xatLabsCheetah:
         resp = requests.get(f"{self.host}/info/device.json")
         self.device_info = resp.json()
     
-    def get_buffer_base64(self):
-        buf = bytearray(self.framebuf)
+    def buffer_to_base64(self, buffer):
+        buf = bytearray(buffer)
         return base64.b64encode(buf).decode('ascii')
+    
+    def update_pixel_buffer(self, image):
+        self.pixel_buffer = [0] * len(self.pixel_buffer)
+        if type(image) in (list, tuple):
+            buf = image[:len(self.pixel_buffer)]
+        else:
+            if not isinstance(image, Image.Image):
+                image = Image.open(image)
+            pixbuf_type = self.display_info.get('pixbuf_type')
+            frame_width = self.display_info.get('frame_width_pixel')
+            frame_height = self.display_info.get('frame_height_pixel')
+            image_width, image_height = image.size
+            if pixbuf_type == '1bpp':
+                image = image.convert('L')
+                pixels = image.load()
+                buf = []
+                for x in range(image_width):
+                    for y in range(0, image_height, 8):
+                        byte = 0
+                        for bit in range(8):
+                            if y + bit < image_height:
+                                byte = (byte >> 1) | ((1 if pixels[x, y + bit] > 127 else 0) << 7)
+                            else:
+                                byte >>= 1
+                        buf.append(byte)
+            else:
+                raise NotImplementedError(f"{pixbuf_type} pixel buffer not yet supported")
+        for i in range(len(self.pixel_buffer)):
+            if i < len(buf):
+                self.pixel_buffer[i] = buf[i]
+            else:
+                self.pixel_buffer[i] = 0
+    
+    def update_text_buffer(self, text):
+        characters = text.encode('iso-8859-1')
+        for i in range(len(self.text_buffer)):
+            if i < len(characters):
+                self.text_buffer[i] = characters[i]
+            else:
+                self.text_buffer[i] = 0
 
-    def set_text(self, text):
-        if self.display_info.get('type') != 'character':
-            raise NotImplementedError("Only character displays support set_text")
-        self.update_framebuf_text(text)
-        if self.host is not None:
-            buf = bytearray(self.framebuf)
-            buffer_b64 = base64.b64encode(buf).decode('ascii')
-            requests.post(f"{self.host}/canvas/buffer.json", json={'buffer': buffer_b64})
+    def update_unit_buffer(self, module_data):
+        self.unit_buffer = [0] * len(self.unit_buffer)
+        for pos, val in module_data.items():
+            self.unit_buffer[pos] = int(val)
 
     def set_brightness(self, brightness):
         if not self.display_info.get('brightness_control'):
@@ -127,15 +128,33 @@ class xatLabsCheetah:
         if self.host is not None:
             requests.post(f"{self.host}/canvas/brightness.json", json={'brightness': brightness})
 
+    def set_image(self, image):
+        if self.display_info.get('pixbuf_size') is None:
+            raise NotImplementedError("Display does not have a pixel buffer")
+        self.update_pixel_buffer(image)
+        if self.host is not None:
+            buffer_b64 = self.buffer_to_base64(self.pixel_buffer)
+            requests.post(f"{self.host}/canvas/buffer/pixel", data=buffer_b64)
+
+    def set_text(self, text):
+        if self.display_info.get('textbuf_size') is None:
+            raise NotImplementedError("Display does not have a text buffer")
+        self.update_text_buffer(text)
+        if self.host is not None:
+            buffer_b64 = self.buffer_to_base64(self.text_buffer)
+            requests.post(f"{self.host}/canvas/buffer/text", data=buffer_b64)
+
     def d_set_module_data(self, module_data):
         # Compatibility function for SplitFlapDisplay class
-        self.update_framebuf_sel(dict(module_data))
+        self.update_unit_buffer(dict(module_data))
 
     def d_update(self):
         # Compatibility function for SplitFlapDisplay class
+        if self.display_info.get('unitbuf_size') is None:
+            raise NotImplementedError("Display does not have a unit buffer")
         if self.host is not None:
-            buffer_b64 = self.get_buffer_base64()
-            requests.post(f"{self.host}/canvas/buffer.json", json={'buffer': buffer_b64})
+            buffer_b64 = self.buffer_to_base64(self.unit_buffer)
+            requests.post(f"{self.host}/canvas/buffer/unit", data=buffer_b64)
 
     def get_splitflap_display(self):
         if self.display_info.get('type') != 'selection':
