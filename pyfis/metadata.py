32620f5a5cc215714c97fb3d5115382b9250f1ac
Julian Metzler <git@mezgr.de>
2026-05-13 14:06:50 +0200

    Change name capitalization to lowercase to fulfill packaging requirements

--- pyfis/metadata.py
+++ pyfis/metadata.py
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-name = "pyFIS"
+name = "pyfis"
 version = "1.15.0"
 description = "A library for controlling devices in the passenger information realm"
 license = "GPLv3"
